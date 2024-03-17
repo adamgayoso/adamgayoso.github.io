65e8f5d67b3a1f45478155c6fc4c7a5d6cd6eff6
adamgayoso <adamgayoso@users.noreply.github.com>
2024-03-17 14:59:37 +0000

    deploy: a9564b1b4a3b97381c61a9f60e35b526f420b415

--- _static/google_analytics_tracker.js
+++ _static/google_analytics_tracker.js
@@ -1,5 +1,2 @@
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-
-gtag('config', 'G-WZL1ZQLZ7K');
+<script data-goatcounter="https://smtp123.goatcounter.com/count"
+    async src="//gc.zgo.at/count.js"></script>
