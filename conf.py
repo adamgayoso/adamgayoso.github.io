c4cdb0b0d523b4e6199b7f96c6cd2a929ac90dff
adamgayoso <adamgayoso@users.noreply.github.com>
2024-06-01 19:19:15 +0100

    update cv

--- conf.py
+++ conf.py
@@ -77,6 +77,7 @@ html_theme = "furo"
 html_title = "Adam Gayoso"
 html_favicon = "./_assets/favicon.ico"
 html_theme_options = {
+    "top_of_page_buttons": None,
     "sidebar_hide_name": False,
     "light_css_variables": {
         # "font-stack": "acumin-pro, sans-serif",
